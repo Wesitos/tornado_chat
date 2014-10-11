add00aed9faa743bbd3711c837ab267d1aa77fcd
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-10-11 03:25:38 -0500

    Modificado para funcionar con chaussette

--- main.py
+++ main.py
@@ -19,33 +19,32 @@ class IndexHandler(BaseHandler):
         chat_list = WebSocketChatHandler.chat_cache
         self.render("index.html", message_list=chat_list)
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/chat/?", IndexRedirectHandler),
-            (r"/chat/index.html", IndexHandler),
-            (r"/chat/login", AuthLoginHandler),
-            (r"/chat/websocket", WebSocketChatHandler),
-            (r"/chat/logout", AuthLogoutHandler),
-        ]
-        settings = {
-            "cookie_secret": options["cookie_secret"],
-            "login_url": r"/chat/login",
-            "xsrf_cookies": True,
-            "facebook_api_key": options.facebook_api_key,
-            "facebook_secret": options.facebook_secret,
-            "debug": True,
-            "ui_modules": uimodules,
-            "static_path":os.path.join(os.path.dirname(__file__), "public"),
-            "static_url_prefix": "/chat/static/",
-            "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-            "autoescape": None,
-        }
-        tornado.web.Application.__init__(self, handlers, **settings)
+handlers = [
+    (r"/chat/?", IndexRedirectHandler),
+    (r"/chat/index.html", IndexHandler),
+    (r"/chat/login", AuthLoginHandler),
+    (r"/chat/websocket", WebSocketChatHandler),
+    (r"/chat/logout", AuthLogoutHandler),
+]
+settings = {
+    "cookie_secret": options["cookie_secret"],
+    "login_url": r"/chat/login",
+    "xsrf_cookies": True,
+    "facebook_api_key": options.facebook_api_key,
+    "facebook_secret": options.facebook_secret,
+    "debug": True,
+    "ui_modules": uimodules,
+    "static_path":os.path.join(os.path.dirname(__file__), "public"),
+    "static_url_prefix": "/chat/static/",
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "autoescape": None,
+}
+
+chat_app = tornado.web.Application(handlers, **settings)
 
 def deploy_server():
     parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
+    tornado.httpserver.HTTPServer(chat_app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
