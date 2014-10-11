b666131e5feda62b0df599699a1246c2f39498b7
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-10-11 01:48:48 -0500

    Agregado path a clear_cookie

--- login.py
+++ login.py
@@ -38,6 +38,6 @@ class AuthLoginHandler(BaseHandler, FacebookGraphMixin):
 
 class AuthLogoutHandler(BaseHandler):
   def get(self):
-    self.clear_cookie("fb_user")
+    self.clear_cookie("fb_user", path="/chat")
     print "Usuario Desautenticado"
     self.redirect(self.get_argument("next", "/chat"))
